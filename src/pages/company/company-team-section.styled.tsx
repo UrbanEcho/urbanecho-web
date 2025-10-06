89e8f60e39c45ed4196a8781fc36f5f2beb15125
Felix Orinda <forinda82@gmail.com>
2025-10-06 23:33:39 +0300

    Implement code changes to enhance functionality and improve performance

--- src/pages/company/company-team-section.styled.tsx
+++ src/pages/company/company-team-section.styled.tsx
@@ -5,56 +5,64 @@ export const CompanyTeamSectionWrapper = styled.section<{ bgColor: string }>`
   background-color: ${({ bgColor }) => bgColor};
   display: flex;
   justify-content: center;
+  padding: ${({ theme }) => `${theme.spacing["40"]} `};
 `;
 
 export const CompanyTeamContent = styled.div<{
   headerColor: string;
   paragraphColor: string;
 }>`
-  width: 800px;
-  max-width: 800px;
+  position: relative;
   display: grid;
   place-items: center;
-  display: relative;
-  overflow: hidden;
-  & .team-section-content {
+  text-align: center;
+  width: 100%;
+  height: 800px;
+  @media (max-width: ${({ theme }) => theme.layout.container.tablet.maxWidth}) {
+    height: 480px;
+  }
+  /* filter: drop-shadow(
+    0px 4px 4px rgba(0, 0, 0, 0.25),
+    0px 8px 8px rgba(0, 0, 0, 0.25),
+    0px 16px 16px rgba(0, 0, 0, 0.25)
+  ); */
+  filter: drop-shadow(
+  -10.91px 11.82px 36.36px rgba(1, 57, 54, 0.17),
+  -43.64px 49.09px 65.45px rgba(1, 57, 54, 0.15),
+  -99.09px 110px 89.09px rgba(1, 57, 54, 0.09),
+  -175.45px 196.36px 105.45px rgba(1, 57, 54, 0.03),
+  -274.55px 306.36px 115.45px rgba(1, 57, 54, 0.00)
+);
+background-blend-mode: multiply;
+  & .team-section-content{
     display: flex;
     flex-direction: column;
     gap: ${({ theme }) => theme.spacing["16"]};
-
-    & h2 {
-      ${({ theme }) => theme.typography.heading["40/medium"]}
-      color: ${({ headerColor }) => headerColor};
-      max-width: 424px;
-      text-align: center;
+    max-width: 26.5rem; /** 424px */
+    z-index: 2;
+    @media (max-width: ${({ theme }) => theme.layout.container.tablet.maxWidth}) {
+      max-width: 13rem; /** 208px */
     }
-
-    & p {
+    h2{
+      ${({ theme }) => theme.typography.heading["40/medium"]}
+      @media (max-width: ${({ theme }) => theme.layout.container.tablet.maxWidth}) {
+        ${({ theme }) => theme.typography.heading["20/medium"]}
+      }
+    }
+    & p{
       ${({ theme }) => theme.typography.paragraph["16/400"]}
       color: ${({ paragraphColor }) => paragraphColor};
-      max-width: 26.5rem; /** 424px */
-      text-align: center;
+      @media (max-width: ${({ theme }) => theme.layout.container.tablet.maxWidth}) {
+        ${({ theme }) => theme.typography.paragraph["14/400"]}
+      }
     }
   }
 `;
-
 
 export const BaseSvgContainer = styled.svg`
   width: 100%;
   height: 100%;
   display: block;
-  filter: drop-shadow(10.91px 11.82px 36.36px rgba(1, 57, 54, 0.17))
-    drop-shadow(43.64px 49.09px 65.45px rgba(1, 57, 54, 0.15))
-    drop-shadow(99.09px 110px 89.09px rgba(1, 57, 54, 0.09))
-    drop-shadow(175.45px 196.36px 105.45px rgba(1, 57, 54, 0.05))
-    drop-shadow(274.55px 306.36px 115.45px rgba(1, 57, 54, 0.03));
-  /* position: absolute; */
-`;
-export const TeamSvgWrapper = styled.g<{ fillColor: string }>`
-  /* & path { */
-  fill: ${({ fillColor }) => fillColor};
-  fill-rule: evenodd;
-  /* } */
-  /* height: 100%;
-  width: 100%; */
+  z-index: 1;
+  position: absolute;
 `;
